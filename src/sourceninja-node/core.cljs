1ec4b97d593476d2fbcecaec526700dc52243747
Matt Stump <mstump@sourceninja.com>
2012-04-25 10:12:58 -0700

    switched to multi-part form post with file upload

--- src/sourceninja-node/core.cljs
+++ src/sourceninja-node/core.cljs
@@ -29,8 +29,11 @@
 (def sn-product-token
   (. env -SOURCENINJA_PRODUCT_TOKEN))
 
+(def boundary
+  "blargl")
+
 (def sn-post-url
-  (str "/products/" sn-product-id "/imports?import_type=node&token=" sn-product-token))
+  (str "/products/" sn-product-id "/imports"))
 
 (defn make-js-map
   "makes a javascript map from a clojure one"
@@ -39,86 +42,96 @@
     (doall (map #(aset out (name (first %)) (second %)) cljmap))
     out))
 
+(defn encode-field-part
+  [boundary name value]
+  (str "--" boundary "\r\n"
+       "Content-Disposition: form-data; name=\"" name "\"\r\n\r\n"
+       value "\r\n"))
+
+(defn encode-file-part
+  [boundary type name filename]
+  (str "--" boundary "\r\n"
+       "Content-Disposition: form-data; name=\"" name "\"; filename=\"" filename "\"\r\n"
+       "Content-Type: " type "\r\n\r\n"))
+
 (defn create-headers
   "create a header hash for the given content string"
-  [content]
-  {"Content-Type" "application/json"
-   "Content-Length" (. content -length)})
+  [content boundary]
+  (make-js-map
+   {"Content-Type" (str "multipart/form-data; boundary=" boundary)
+    "Content-Length" (. content -length)}))
 
 (defn post-options
-  [json]
+  [headers]
   (make-js-map
    {:hostname sn-post-host
     :port sn-post-port
     :path sn-post-url
     :method "POST"
-    :headers (make-js-map (create-headers json))}))
+    :headers headers}))
 
 (defn json-generate
   [data]
   "not good for general purpose, can't find a good general clj->js function, only works for maps"
-  (str (JSON/stringify (map make-js-map data)) "\n"))
+  (str (JSON/stringify (apply array (map make-js-map data))) "\n"))
 
-(defn extract-name-and-version
+(defn format-deps
   "Take the intermediate output and create a set of
    hashes. Each hash contains a name and a version"
-  [input]
+  [input direct]
   (for [[name data] input]
     {"name" name
-     "version" (get data "version")}))
+     "version" (get data "version")
+     "direct" (contains? direct name)}))
 
 (defn flatten-deps
   "Consume the graph that we get from NPM and spit out a hash of hashes.
    Each hash contains a name and a version, duplicates are eliminated"
   [input]
-  (extract-name-and-version
-   (loop [output {}
-          input input]
+  (loop [output {}
+         input input]
 
-     (if-not (empty? input)
+    (if-not (empty? input)
 
-       (let [notseen (remove (partial contains? output) (keys input))
-             newout (reduce #(assoc %1 %2 (get input %2)) output notseen)
-             ndeps (apply merge (map #(get (get input %1) "dependencies") notseen))]
+      (let [notseen (remove (partial contains? output) (keys input))
+            newout (reduce #(assoc %1 %2 (get input %2)) output notseen)
+            ndeps (apply merge (map #(get (get input %1) "dependencies") notseen))]
 
-         (recur newout ndeps))
-       output))))
+        (recur newout ndeps))
+      output)))
 
 (defn conn-response-handler
   [res]
   (println (str "status: " (. res -statusCode)))
   (println (str "headers: " (js->clj (. res -headers)))))
 
-(defn conn-error-handler
-  [e]
-  (if (= (. e -errno) (. js/process -ECONNREFUSED))
-    (println (str "ECONNREFUSED: connection refused to "
-                  post-host
-                  ":"
-                  post-port))
-
-    (println "blargl " (js->clj e))))
-
 (defn post-deps
   [deps]
-  (let [json (json-generate deps)
-        options (post-options json)
+  (let [json (json-generate (format-deps (flatten-deps deps) (set (keys deps))))
+        form_data (str (encode-field-part boundary "token" sn-product-token)
+                       (encode-field-part boundary "meata_source_type" "node")
+                       (encode-field-part boundary "import_type" "json")
+                       (encode-file-part boundary "application/json" "import" "node.json")
+                       json
+                       "\r\n--" boundary "--")
+        headers (create-headers form_data boundary)
+        options (post-options headers)
         request (.request https options conn-response-handler)]
 
-    (.write request json)
+    (.write request form_data)
     (.end request)))
 
 (defn npm-ls-callback
   [_ _ lite]
-  (let [deps (flatten-deps (js->clj (. js/lite -dependencies)))]
+  (let [deps (js->clj (. js/lite -dependencies))]
     (post-deps deps)))
 
 (defn load-npm-callback
   [_ npm]
   (.ls (. js/npm -commands) [] true npm-ls-callback))
 
-(defn kaboom
+(defn kapow
   []
   (.load npm load-npm-callback))
 
-(set! *main-cli-fn* kaboom)
+(set! *main-cli-fn* kapow)
